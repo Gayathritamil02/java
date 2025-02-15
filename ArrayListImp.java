df72a77da8bda6c71c4ed3bef1d904d6f5c44d07
Gayathritamil02 <gayatritamil2002@gmail.com>
2025-02-15 07:35:31 +0200

    Add files via upload

---
+++ ArrayListImp.java
@@ -0,0 +1,17 @@
+package collection;
+import java.util.*;
+import java.util.ArrayList;
+public class ArrayListImp {
+	public static void main(String args[]) {
+		ArrayList<String> names=new ArrayList<String>();
+		names.add("gayu");
+		names.add("kavi");
+		names.add("sri");
+		names.add("Arthi");
+		names.add("priya");
+		for(String name:names) {
+			System.out.println(names);
+	}
+
+}
+}
