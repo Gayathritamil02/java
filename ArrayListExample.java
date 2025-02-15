39a81763bf1d06d18a905592d4ca349dfb83cc46
Gayathritamil02 <gayatritamil2002@gmail.com>
2025-02-15 07:34:23 +0200

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.ArrayList;
+public class ArrayListExample {
+public static void main(String args[]){
+ArrayList<String> a=new ArrayList<String>();
+a.add("Red");
+a.add("Green");
+a.add("Blue");
+a.add("Orange");
+a.add("Violet");
+System.out.println(a);
+}
+}
